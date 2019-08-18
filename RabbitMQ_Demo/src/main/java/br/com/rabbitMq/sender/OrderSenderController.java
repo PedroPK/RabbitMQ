7d74c15ab79d7fea0e4ab9117288bf7604e62dc9
pedropk <pedropk@gmail.com>
2019-08-18 17:33:04 -0300

    Sender Controller created

---
+++ RabbitMQ_Demo/src/main/java/br/com/rabbitMq/sender/OrderSenderController.java
@@ -0,0 +1,31 @@
+package br.com.rabbitMq.sender;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping(value="/orders")
+@Slf4j
+public class OrderSenderController {
+	
+	@Autowired
+	private OrderQueueSender aOrderQueueSender;
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public void send(
+		@RequestBody
+		String pOrder
+	) {
+		log.info("Order will be send");
+		
+		this.aOrderQueueSender.send(pOrder);
+		
+		log.info("Order was sent");
+	}
+	
+}
