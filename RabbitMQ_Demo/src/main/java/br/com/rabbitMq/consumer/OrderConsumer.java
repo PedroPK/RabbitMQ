c531530f92e720bcbc6997fad6e7c27adc89a7c5
pedropk <pedropk@gmail.com>
2019-08-18 17:11:54 -0300

    Initial commit with RabbitMQ implementation
    It needs to be tested yet

---
+++ RabbitMQ_Demo/src/main/java/br/com/rabbitMq/consumer/OrderConsumer.java
@@ -0,0 +1,35 @@
+package br.com.rabbitMq.consumer;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+/*
+ * Once tue Consumer has to be mapped into Spring container,
+ * it has to receive the Component annotation
+ */
+@Component
+
+/*
+ * Lombok annotation, allowing us to Log with less effort 
+ */
+@Slf4j
+public class OrderConsumer {
+	
+	/**
+	 * The @RabbitListener annotation marks this method as a Listener
+	 * 
+	 * The @Payload annotation informs that a parameter is going to 
+	 * receive the Message Body. 
+	 *  - If the method was only one parameter, this annotation is not necessary
+	 * 
+	 * @param pFileBody
+	 */
+	@RabbitListener(queues = {"$queue.order.name"})
+	public void receive(@Payload String pFileBody) {
+		log.info(pFileBody);
+	}
+	
+}
---
+++ RabbitMQ_Demo/src/main/java/br/com/rabbitMq/sender/OrderQueueSender.java
@@ -0,0 +1,34 @@
+package br.com.rabbitMq.sender;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderQueueSender {
+	
+	/**
+	 * Helper class to access and Send messages to RabbitMQ 
+	 *  - Default behavior is synchronous
+	 */
+	@Autowired
+	private RabbitTemplate aRabbitTemplate;
+	
+	/**
+	 * Object that represents a setup Queue
+	 */
+	@Autowired
+	private Queue aQueue;
+	
+	public void send(String pOrder) {
+		/*
+		 * Method that receives a Routing Key and a Message.
+		 *  - The Routing Key is the Queue Name
+		 */
+		this.aRabbitTemplate.convertAndSend(
+			this.aQueue.getName(), 
+			pOrder);
+	}
+	
+}
